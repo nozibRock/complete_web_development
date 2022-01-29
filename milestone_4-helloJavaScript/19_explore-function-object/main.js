1f00d31092aff9199eb74bfb34577b5d78fed814
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-01-29 19:10:19 +0600

    add object

--- milestone_4-helloJavaScript/19_explore-function-object/main.js
+++ milestone_4-helloJavaScript/19_explore-function-object/main.js
@@ -103,3 +103,60 @@ function divideTwoNumbers(num1, num2) {
 
 var totalDivision = divideTwoNumbers(5, 100);
 console.log('total after Division: ', totalDivision);
+
+
+// object
+var student1 = {
+    id: 115,
+    name: 'Abir Adnan',
+    marks: 81,
+    phone: +8801922438860
+};
+
+var mobile = {
+    color: 'black',
+    price: 19000,
+    screenSize: 5,
+    storage: '16gb'
+};
+
+var book = {
+    title: 'Start With Why',
+    author: 'Simon Sinek',
+    pages: 246,
+    publisher: 'Portfolio Penguin',
+    prices: 1078
+}
+
+// object-properties
+var computer = {
+    price: 29000,
+    storage: '256gb',
+    color: 'silver',
+    processor: 'intel i5'
+}
+
+// console.log(computer);
+// console.log(computer.processor);
+
+var computerPrice = computer.price;
+// console.log(computerPrice);
+
+// set a object property value
+computer.price = 22000;
+// console.log(computer);
+// console.log(computerPrice);
+
+// different ways to set a value of an object property
+var priceProperty = "price";
+
+computer.price = 22000;
+computer["price"] = 23000;
+computer[priceProperty] = 19000;
+
+var storageProperty = 'storage';
+computer[storageProperty] = '512gb';
+computer['storage'] = '1tb';
+computer.storage = '5tb';
+
+console.log(computer);
