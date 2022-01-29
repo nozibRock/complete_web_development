db094c38bc8bb9d7e64dda3d42191ee67344ae39
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-01-29 12:12:32 +0600

    add fuction

--- milestone_4-helloJavaScript/19_explore-function-object/main.js
+++ milestone_4-helloJavaScript/19_explore-function-object/main.js
@@ -31,3 +31,75 @@ while (i < 15) {
 for (var i = 0; i < 15; i++) {
     // console.log(i);
 }
+
+// function
+function startFan() {
+    console.log('walk towards the switch')
+    console.log('press the switch');
+}
+// call the function
+// startFan();
+
+// console.log('eat breakfast');
+// console.log('Drink tea');
+// console.log('take a shower');
+// startFan();
+var country = "bd";
+var city = 'Dhaka';
+var location = 'Gulistan';
+// startFan();
+
+
+// parameter
+function bringSingara(taka) {
+    console.log('singara er jonno', taka, 'tk dise');
+    console.log('Mama singara den');
+    var singaraPrice = 10;
+    var singaraQuantity = taka / singaraPrice;
+    return singaraQuantity;
+}
+// console.log('ei nen', bringSingara(250), 'ta singara');
+
+var money = 250;
+// var singara = bringSingara(money);
+// console.log('ei nen', singara, 'ta singara');
+
+// multiple-parameter
+function addTwoNumbers(number1, number2) {
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+
+// console.log('result value: ', addTwoNumbers(21, 59));
+
+var firstNumber = 35;
+var secondNumber = 47;
+var result = addTwoNumbers(secondNumber, firstNumber);
+console.log('result value: ', result);
+
+function multiplyTwoNumbers(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+var total = multiplyTwoNumbers(5, 100);
+console.log('total after multiplication', total);
+
+// biyog koro duita sonkha
+function subtractionTwoNumbers(num1, num2) {
+    var subtractionResult = num2 - num1;
+    return subtractionResult;
+}
+
+var total = subtractionTwoNumbers(5, 100);
+console.log('total after subtraction', total);
+
+// vabfol ber koro
+function divideTwoNumbers(num1, num2) {
+    var division = num2 / num1;
+    return division;
+}
+
+var totalDivision = divideTwoNumbers(5, 100);
+console.log('total after Division: ', totalDivision);
