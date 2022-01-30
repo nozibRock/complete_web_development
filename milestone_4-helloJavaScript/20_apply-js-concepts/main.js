223d58b7c61ea021f85ae86630bf9506fdf5566d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-01-30 18:58:35 +0600

    add leap-year

--- milestone_4-helloJavaScript/20_apply-js-concepts/main.js
+++ milestone_4-helloJavaScript/20_apply-js-concepts/main.js
@@ -119,3 +119,21 @@ function isEvenOrOdd(number) {
 
 isEvenOrOdd(1641);
 isEvenOrOdd(1892);
+
+// leap-year
+function isLeapYear(year) {
+    if (year % 4 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const myYear = 2100;
+const isMyYearLeapYear = isLeapYear(myYear);
+console.log('Is my year leap year', isMyYearLeapYear);
+
+const yourYear = 2224;
+const isYourYearLeapYear = isLeapYear(yourYear);
+console.log('is your year leap year', isYourYearLeapYear);
