43fe7ee9cfbf9ffae226a603803f2520a52bbe0f
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-01-31 07:23:34 +0600

    add factorials

--- milestone_4-helloJavaScript/20_apply-js-concepts/main.js
+++ milestone_4-helloJavaScript/20_apply-js-concepts/main.js
@@ -137,3 +137,92 @@ console.log('Is my year leap year', isMyYearLeapYear);
 const yourYear = 2224;
 const isYourYearLeapYear = isLeapYear(yourYear);
 console.log('is your year leap year', isYourYearLeapYear);
+
+
+// factorial
+var factorial = 1;
+for (var i = 1; i <= 7; i++) {
+    factorial = factorial * i;
+}
+console.log(factorial);
+
+
+function getFactorial(factorialNumber) {
+    let factorialX = 1;
+    for (let i = 1; i <= factorialNumber; i++) {
+        factorialX = factorialX * i;
+    }
+    return factorialX;
+}
+
+var firstFactorial = getFactorial(7);
+console.log('factorial of 7 is ', firstFactorial);
+
+var secondFactorial = getFactorial(9);
+console.log('Factorial of 9 is ', secondFactorial);
+
+
+// factorial-while-loop
+/* function getFactorialWhile(factNumWhile) {
+    let factorialNum = 1;
+    let w = 1;
+    while (w <= factNumWhile) {
+        factorialNum = factorialNum * w;
+        w++;
+    }
+    return factorialNum;
+}
+
+const myFactorial = getFactorialWhile(6);
+console.log(myFactorial); */
+
+/* function getFactorial(number) {
+    let factorial = 1;
+    let i = number;
+    while (i >= 1) {
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial
+}
+const myFactorial = getFactorial(6);
+console.log(myFactorial); */
+
+// for loop reverse
+/* function getFactorial2(number) {
+    let factorial = 1;
+    for (let i = number; i >= 1; i--) {
+        factorial = factorial *i;
+    }
+    return factorial;
+}
+
+const myFactorial = getFactorial2(6);
+console.log(myFactorial); */
+
+
+// factorial-recursion
+function factorial(n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+const myFactorial = factorial(5);
+console.log(myFactorial);
+
+
+// debug
+function getFactorial(number) {
+    let factorial = 1;
+    // console.log(number);
+    for (let i = 1; i <= number; i++) {
+        factorial = factorial * i;
+        console.log(i, factorial);
+    }
+    return factorial;
+}
+
+const getMyFactorial = getFactorial(8);
+console.log(getMyFactorial);
