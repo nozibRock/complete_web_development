07c69ce569de301475738245ae3e3fb530979198
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-01-31 09:22:21 +0600

    add practice problems

---
+++ milestone_4-helloJavaScript/20_apply-js-concepts/practice_20.js
@@ -0,0 +1,29 @@
+// leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
+function leapYear(year) {
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+leapYear(2023);
+
+// তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে।
+function isEvenOrOdd(num) {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+var myAge = isEvenOrOdd(27);
+console.log(myAge);
+
+
+// এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে।
+function hoursToMinutes(hours) {
+    var result = hours * 60;
+    return result;
+}
+var minutes = hoursToMinutes(9);
+console.log(minutes, "minutes");
