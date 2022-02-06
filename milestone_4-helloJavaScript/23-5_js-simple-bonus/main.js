ede267041af6b92384ac650a73c445919c833988
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-06 15:40:34 +0600

    add basic search functionalities and break and continue

--- milestone_4-helloJavaScript/23-5_js-simple-bonus/main.js
+++ milestone_4-helloJavaScript/23-5_js-simple-bonus/main.js
@@ -1,40 +1,40 @@
 // simple
-/* for (let index = 1; index <= 5; index++) {
+for (let index = 1; index <= 5; index++) {
     console.log(index);
-} */
+}
 
-/* function consoleNumber(i) {
+function consoleNumber(i) {
     if (i > 5) {
         return i;
     }
     console.log(i);
     consoleNumber(i+1);
 }
-consoleNumber(1); */
+consoleNumber(1);
 
 
 // sum
-/* let sum = 0;
+let sum = 0;
 for (let index = 5; index >= 1; index--) {
     sum = sum + index;
 }
-console.log(sum); */
+console.log(sum);
 
-/* function sum(i) {
+function sum(i) {
     if (i == 1) {
         return 1;
     }
     return i + sum(--i);
 }
-console.log(sum(5)); */
+console.log(sum(5));
 
 
 // factorial
-/* let factorial = 1;
+let factorial = 1;
 for (let index = 4; index >= 1; index--) {
     factorial = factorial * index;
 }
-console.log(factorial); */
+console.log(factorial);
 
 function factorial(i) {
     if (i == 1) {
@@ -43,3 +43,37 @@ function factorial(i) {
     return i * factorial(--i);
 }
 console.log(factorial(4));
+
+
+// search
+const products = [
+  { name: "iphone 14", price: 60000 },
+  { name: "samsung galaxy 14", price: 18000 },
+  { name: "dell laptop", price: 50000 },
+  { name: "lenovo yoga laptop", price: 55000 },
+  { name: "Asus laptop", price: 45000 },
+  { name: "Smart watch samsung", price: 15000 },
+  { name: "Apple watch", price: 12000 },
+  { name: "One Plus phone 4", price: 41000 },
+];
+
+function searchProducts(products, searchText) {
+    for (const product of products) {
+        if (product.name.includes(searchText)) {
+          console.log(product.name);
+        }
+    }
+}
+searchProducts(products, "phone");
+console.log("matched");
+
+
+// break-down
+for (const product of products) {
+    if (product.price < 40000) {
+        // break;
+        continue;
+    }
+    console.log(product);
+}
+console.log("after the loop");
