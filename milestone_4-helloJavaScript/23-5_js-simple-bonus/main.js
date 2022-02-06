602b9c0caa69185dddc04bdd1de49dbe3b18ec2f
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-06 13:44:40 +0600

    sum

--- milestone_4-helloJavaScript/23-5_js-simple-bonus/main.js
+++ milestone_4-helloJavaScript/23-5_js-simple-bonus/main.js
@@ -11,3 +11,19 @@ function consoleNumber(i) {
     consoleNumber(i+1);
 }
 consoleNumber(1);
+
+
+// sum
+let sum = 0;
+for (let index = 5; index >= 1; index--) {
+    sum = sum + index;
+}
+console.log(sum);
+
+function sum(i) {
+    if (i == 1) {
+        return 1;
+    }
+    return i + sum(--i);
+}
+console.log(sum(5));
