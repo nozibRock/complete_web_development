e732a4864b6ad0f821676d75ae027d376467b95f
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-06 13:59:38 +0600

    add recusive factorial

--- milestone_4-helloJavaScript/23-5_js-simple-bonus/main.js
+++ milestone_4-helloJavaScript/23-5_js-simple-bonus/main.js
@@ -1,29 +1,45 @@
 // simple
-for (let index = 1; index <= 5; index++) {
+/* for (let index = 1; index <= 5; index++) {
     console.log(index);
-}
+} */
 
-function consoleNumber(i) {
+/* function consoleNumber(i) {
     if (i > 5) {
         return i;
     }
     console.log(i);
     consoleNumber(i+1);
 }
-consoleNumber(1);
+consoleNumber(1); */
 
 
 // sum
-let sum = 0;
+/* let sum = 0;
 for (let index = 5; index >= 1; index--) {
     sum = sum + index;
 }
-console.log(sum);
+console.log(sum); */
 
-function sum(i) {
+/* function sum(i) {
     if (i == 1) {
         return 1;
     }
     return i + sum(--i);
 }
-console.log(sum(5));
+console.log(sum(5)); */
+
+
+// factorial
+/* let factorial = 1;
+for (let index = 4; index >= 1; index--) {
+    factorial = factorial * index;
+}
+console.log(factorial); */
+
+function factorial(i) {
+    if (i == 1) {
+        return 1;
+    }
+    return i * factorial(--i);
+}
+console.log(factorial(4));
