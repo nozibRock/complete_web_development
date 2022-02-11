41aaa710a3ea437ecb5da3578718bc2592ccbe9b
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-11 18:40:40 +0600

    handle withdraw event handler and update balance

--- milestone_5-integrated-javascript/26_simple_bank/js/banking.js
+++ milestone_5-integrated-javascript/26_simple_bank/js/banking.js
@@ -26,3 +26,31 @@ document
     // clear the deposit input field
     depositInput.value = "";
   });
+
+// handle withdraw event handler
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    // set withdraw total
+    const withdrawTotal = document.getElementById("withdraw-total");
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById("balance-total");
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear withdraw input
+    withdrawInput.value = "";
+  });
