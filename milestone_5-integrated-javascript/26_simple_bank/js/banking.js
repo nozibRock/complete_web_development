227f9beef2e55df168076529bc5bb32ae8ebae7c
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-11 11:55:09 +0600

    update deposit balance

--- milestone_5-integrated-javascript/26_simple_bank/js/banking.js
+++ milestone_5-integrated-javascript/26_simple_bank/js/banking.js
@@ -1,18 +1,28 @@
 // handle deposit button event
-document.getElementById("deposit-button").addEventListener('click', function () {
-  console.log("deposit button clicked");
-  // get the amount deposited
-  const depositInput = document.getElementById("deposit-input");
-  const newDepositAmountText = depositInput.value;
-  console.log(newDepositAmountText);
-  const newDepositAmount = parseFloat(newDepositAmountText);
-  console.log(newDepositAmount);
+document
+  .getElementById("deposit-button")
+  .addEventListener("click", function () {
+    console.log("deposit button clicked");
+    // get the amount deposited
+    const depositInput = document.getElementById("deposit-input");
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
 
-  // update deposit total
-  const depositTotal = document.getElementById("deposit-total");
-  const previousDepositText = depositTotal.innerText;
-  const previousDepositAmount = parseFloat(previousDepositText);
-  const newDepositTotal = previousDepositAmount + newDepositAmount;
+    // update deposit total
+    const depositTotal = document.getElementById("deposit-total");
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
 
-  depositTotal.innerText = newDepositTotal;
-});
+    depositTotal.innerText = newDepositTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear the deposit input field
+    depositInput.value = "";
+  });
