bc25dbfed117c02a810f0c19060f8bef4115c846
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-12 10:14:02 +0600

    get user deposit and set deposit total value

---
+++ milestone_5-integrated-javascript/27_functional-bank/js/banking.js
@@ -0,0 +1,15 @@
+document.getElementById("deposit-button").addEventListener('click', function () {
+    console.log("deposit button clicked");
+    const depositInput = document.getElementById("deposit-input");
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+
+    // get current deposit
+    const depositTotal = document.getElementById("deposit-total");
+    const depositTotalText = depositTotal.innerText;
+    depositTotal.innerText = depositAmount;
+    console.log(depositTotalText);
+
+    // clear input field
+    depositInput.value = '';
+});
