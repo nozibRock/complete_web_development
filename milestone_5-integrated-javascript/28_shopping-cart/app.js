4125f482f4c1db9a36515b1c11d69e562ca6466b
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-14 11:28:29 +0600

    Get all product sub total cost, tax and grand total

--- milestone_5-integrated-javascript/28_shopping-cart/app.js
+++ milestone_5-integrated-javascript/28_shopping-cart/app.js
@@ -1,24 +1,43 @@
-// console.log("I am in a separate file");
-
 function updateProductNumber(product, price, isIncreasing) {
   const productInput = document.getElementById(product + "-number");
   let productNumber = productInput.value;
   if (isIncreasing == true) {
-     productNumber = parseInt(productNumber) + 1;
+    productNumber = parseInt(productNumber) + 1;
   } else if (productNumber > 0) {
-     productNumber = parseInt(productNumber) - 1;
+    productNumber = parseInt(productNumber) - 1;
   }
 
-  productInput.value = productNumber; 
+  productInput.value = productNumber;
   // update case total
-  const caseTotal = document.getElementById(product + "-total");
-  caseTotal.innerText = productNumber * price;
+  const productTotal = document.getElementById(product + "-total");
+  productTotal.innerText = productNumber * price;
+
+  // calculate total
+  calculateTotal();
 }
+function getInputValue(product) {
+  const productInput = document.getElementById(product + "-number");
+  const productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+function calculateTotal() {
+  const phoneTotal = getInputValue("phone") * 796;
+  const caseTotal = getInputValue("case") * 59;
+  const subTotal = phoneTotal + caseTotal;
+  const tax = subTotal / 10;
+  const totalPrice = subTotal + tax;
+
+  // update on the html
+  document.getElementById("sub-total").innerText = subTotal;
+  document.getElementById("tax-amount").innerText = tax;
+  document.getElementById("total-price").innerText = totalPrice;
+}
+
 // phone increase decrease events
-document.getElementById('phone-plus').addEventListener('click', function () {
+document.getElementById("phone-plus").addEventListener("click", function () {
   console.log("phone plus clicked");
   updateProductNumber("phone", 796, true);
-})
+});
 document.getElementById("phone-minus").addEventListener("click", function () {
   updateProductNumber("phone", 796, false);
 });
@@ -27,5 +46,5 @@ document.getElementById("case-plus").addEventListener("click", function () {
   updateProductNumber("case", 59, true);
 });
 document.getElementById("case-minus").addEventListener("click", function () {
-    updateProductNumber("case", 59, false);
+  updateProductNumber("case", 59, false);
 });
