c72ceed3ecb22c4e9c7fd1154b745e4fb08072ad
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-14 11:09:38 +0600

    calculate phone cost using the same shared function

--- milestone_5-integrated-javascript/28_shopping-cart/app.js
+++ milestone_5-integrated-javascript/28_shopping-cart/app.js
@@ -1,22 +1,31 @@
 // console.log("I am in a separate file");
 
-function updateCaseNumber(isIncreasing) {
-  const caseInput = document.getElementById("case-number");
-  let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+  const productInput = document.getElementById(product + "-number");
+  let productNumber = productInput.value;
   if (isIncreasing == true) {
-     caseNumber = parseInt(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-     caseNumber = parseInt(caseNumber) - 1;
+     productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+     productNumber = parseInt(productNumber) - 1;
   }
 
-  caseInput.value = caseNumber; 
+  productInput.value = productNumber; 
   // update case total
-  const caseTotal = document.getElementById("case-total");
-  caseTotal.innerText = caseNumber * 59;
+  const caseTotal = document.getElementById(product + "-total");
+  caseTotal.innerText = productNumber * price;
 }
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+  console.log("phone plus clicked");
+  updateProductNumber("phone", 796, true);
+})
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateProductNumber("phone", 796, false);
+});
+// handle case increase decrease events
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updateProductNumber("case", 59, true);
 });
 document.getElementById("case-minus").addEventListener("click", function () {
-    updateCaseNumber(false);
+    updateProductNumber("case", 59, false);
 });
