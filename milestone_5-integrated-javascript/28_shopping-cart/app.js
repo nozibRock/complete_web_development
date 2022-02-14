f7fdcecf96b36cb9125484235870a8db22084b15
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-14 10:17:56 +0600

    shopping cart handle decrease and update price

--- milestone_5-integrated-javascript/28_shopping-cart/app.js
+++ milestone_5-integrated-javascript/28_shopping-cart/app.js
@@ -1,9 +1,22 @@
 // console.log("I am in a separate file");
 
-document.getElementById("case-plus").addEventListener('click', function () {
-    // console.log('case-plus clicked');
-    const caseInput = document.getElementById("case-number");
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-    // console.log(caseNumber);
+function updateCaseNumber(isIncreasing) {
+  const caseInput = document.getElementById("case-number");
+  let caseNumber = caseInput.value;
+  if (isIncreasing == true) {
+     caseNumber = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+     caseNumber = parseInt(caseNumber) - 1;
+  }
+
+  caseInput.value = caseNumber; 
+  // update case total
+  const caseTotal = document.getElementById("case-total");
+  caseTotal.innerText = caseNumber * 59;
+}
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+    updateCaseNumber(false);
 });
