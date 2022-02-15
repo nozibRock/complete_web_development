631544482961c47742ad57c676ee686e21d76bc2
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-15 22:49:03 +0600

    Get part of an array and insert elements using slice, splice

--- milestone_5-integrated-javascript/29_js-string-array/main.js
+++ milestone_5-integrated-javascript/29_js-string-array/main.js
@@ -120,5 +120,22 @@ if (friends.includes("Lion") == true) {
 const firstArray = [1, 3, 5];
 const secondArray = [4, 6, 8, 9];
 const combined = firstArray.concat(secondArray);
-console.log(combined);
+// console.log(combined);
 
+
+
+const numbers = [3, 6, 4, 8, 9, 19, 15, 21, 45, 87];
+//slice
+// const numberSliced = numbers.slice(4, 8);
+// console.log(numbers);
+// console.log(numberSliced);
+// console.log(numbers);
+
+//splice to remove an element from an array
+// const numberSpliced = numbers.splice(4, 3);
+// console.log(numberSpliced);
+// console.log(numbers);
+console.log(numbers);
+const numberSpliced2 = numbers.splice(4, 3, 99, 111, 777, 888, 999);
+console.log(numberSpliced2);
+console.log(numbers);
