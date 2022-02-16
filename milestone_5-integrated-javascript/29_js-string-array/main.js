ed151b687d87891786e2487ab462a0a9b29416f8
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-16 11:23:45 +0600

    Different types of error object and explore try-catch

--- milestone_5-integrated-javascript/29_js-string-array/main.js
+++ milestone_5-integrated-javascript/29_js-string-array/main.js
@@ -186,4 +186,25 @@ const name = getFullName(
   "Kumuk",
   "Songket"
 );
-console.log(name);
+// console.log(name);
+
+
+// try-catch
+const myName = 'ami';
+const student = undefined;
+
+// console.log(student.name);
+try {
+    // myName = 'tumi';
+}
+catch (error) {
+    console.log('getting error', error);
+}
+console.log(100);
+console.log(myName);
+
+// for (let i = 0; i < 10 i++) {
+
+// }
+// name = 'tumi';
+// console.log(name);
