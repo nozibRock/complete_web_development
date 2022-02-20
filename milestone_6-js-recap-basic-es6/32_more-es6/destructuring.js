5a097c87021e21ac1109f1027f3116f9d665de76
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-20 14:58:16 +0600

    add array map

---
+++ milestone_6-js-recap-basic-es6/32_more-es6/destructuring.js
@@ -0,0 +1,48 @@
+const fish = {
+  id: 58,
+  name: "King Hilsha",
+  price: 9000,
+  phone: "01717555555555",
+  address: "Chandpur",
+  dress: "silver",
+};
+
+// console.log(fish.phone);
+// console.log(fish.phone);
+// console.log(fish.phone);
+// console.log(fish.phone);
+// console.log(fish.phone);
+// const phone = fish.phone;
+// const price = fish.price;
+// const dress = fish.dress;
+// const id = fish.id;
+
+const { phone, price, dress, id } = fish;
+
+// console.log(phone, price);
+// console.log(phone, id);
+// console.log(phone, dress);
+// console.log(phone);
+
+const company = {
+  name: "GP",
+  ceo: { id: 1, name: "Yasir Azman", food: "fuchka" },
+  web: {
+    work: "website development",
+    employee: 22,
+    framework: "react",
+    tech: {
+      first: "html",
+      second: "css",
+      third: "js",
+    },
+  },
+};
+
+
+// const work = company.web.work;
+// const framework = company.web.framework;
+const { work, framework } = company.web;
+const { food } = company.ceo;
+const { second, third } = company.web.tech;
+console.log(work, framework, food);
