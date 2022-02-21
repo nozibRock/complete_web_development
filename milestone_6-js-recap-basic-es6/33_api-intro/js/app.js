1171005dd0d5f8eb3e8a0d8ef1655c130c490e17
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-21 23:07:54 +0600

    API JSON, JSON structure, parse, stringify

---
+++ milestone_6-js-recap-basic-es6/33_api-intro/js/app.js
@@ -0,0 +1,45 @@
+console.log("API");
+// javascript Object Notation
+// JSON
+const user = { id: 11, userName: "Aamir Khan", job: "actor" };
+const stringified = JSON.stringify(user);
+console.log(stringified);
+console.log(user);
+
+const shop = {
+  name: "Alia Store",
+  address: "Ranbir road",
+  products: ["laptop", "mobile", "pepsi"],
+  owner: {
+    ownerName: "Alia Bhatt",
+    profession: "actor",
+  },
+  profit: 150000,
+  isExpensive: false,
+};
+
+const shopStringified = JSON.stringify(shop);
+console.log(shop);
+console.log(shopStringified);
+
+const converted = JSON.parse(shopStringified)
+console.log(converted);
+console.log(converted.owner);
+
+const student = {
+    id: 1001, 
+    name: "Alex",
+    email: 'alex@gmail.com',
+    password: "alex@123"
+}
+
+const studentStr1 = JSON.stringify(student, function (key, value) {
+    if (key == 'password') {
+        return undefined;
+    } else {
+        return value;
+    }
+});
+console.log(studentStr1);
+const studentStr2 = JSON.stringify(student, ['id', 'name', 'email']);
+console.log(studentStr2);
