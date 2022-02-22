02805632d5855e217adfeddfb95ac4e66f1af4a3
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-22 11:00:41 +0600

    Debug API, Network tab,Status code, headers, bad API

--- milestone_6-js-recap-basic-es6/33_api-intro/js/posts.js
+++ milestone_6-js-recap-basic-es6/33_api-intro/js/posts.js
@@ -24,3 +24,20 @@ function displayPosts(posts) {
     console.log(post);
   }
 }
+
+
+function addAPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: 1,
+            title: 'My new post',
+            body: 'This is my posts'
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
