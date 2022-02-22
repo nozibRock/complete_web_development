3cc6e71475e466aa25bc7b3ee79b144d8a0b2525
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-22 08:53:57 +0600

    Load more data, APIs and send data to function

--- milestone_6-js-recap-basic-es6/33_api-intro/js/api.js
+++ milestone_6-js-recap-basic-es6/33_api-intro/js/api.js
@@ -1,7 +1,26 @@
 console.log("api.js connected");
 
 function loadData() {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
+  fetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  )
     .then((response) => response.json())
     .then((data) => console.log(data));
+}
+
+function loadUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    // .then((data) => console.log(data));
+    .then((data) => displayUsers(data));
+}
+
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function displayUsers(data) {
+  console.log(data);
 }
