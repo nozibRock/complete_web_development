408487c14ba8220815c3546084225d86ca2b6e8d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-24 17:23:41 +0600

    details modal bug fix :v

--- milestone_6-js-recap-basic-es6/34_api-examples/js/mealdb.js
+++ milestone_6-js-recap-basic-es6/34_api-examples/js/mealdb.js
@@ -1,25 +1,27 @@
 const searchFood = () => {
   const searchField = document.getElementById("searchField");
   const searchText = searchField.value;
-//   console.log(searchText);
+  //   console.log(searchText);
 
   searchField.value = "";
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-//   console.log(url);
+  //   console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => displaySearchResults(data.meals));
 };
 
 const displaySearchResults = (meals) => {
-//   console.log(meals);
+  //   console.log(meals);
   const searchResult = document.getElementById("search-result");
   meals.forEach((meal) => {
     // console.log(meal);
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-            <div onClick=" loadMealDetail( ${meal.idMeal} ) "  class="card h-100">
+            <div onClick=" loadMealDetail( ${
+              meal.idMeal
+            } ) "  class="card h-100">
                 <img src="${meal.strMealThumb} " class="card-img-top" alt="${
       meal.strMeal
     }">
@@ -29,7 +31,9 @@ const displaySearchResults = (meals) => {
                       0,
                       250
                     )} </p>
-                    <button type="button" class="btn btn-info" onClick="loadMealDetail()" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Meal Details</button>
+                    
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick="loadMealDetail()">Launch details</button>
+
                 </div>
             </div>
         `;
@@ -46,27 +50,15 @@ const loadMealDetail = (mealId) => {
 };
 
 const displayMealDetails = (meal) => {
-  const mealDetails = document.getElementById("staticBackdrop");
-  //   mealDetails.innerHTML = "";
+  const mealDetails = document.getElementById("meal-details");
+  mealDetails.innerHTML = "";
   const div = document.createElement("div");
-  div.classList.add("modal-dialog");
   div.innerHTML = `
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${meal.strMeal}</h5>
-            <button id="meal-details" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <img src="${meal.strMealThumb} " class="card-img-top" alt="${meal.strMeal}">
-            <p class="card-text">Meal Origin: ${meal.strArea} </p>
-            <p class="card-text"> Category: ${meal.strCategory} </p>
-            <p class="card-text"> ${meal.strInstructions} </p>
-            <a href=" ${meal.strYoutube} ">Video recipe</a>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        </div> 
-    </div>
+    <img src="${meal.strMealThumb} " class="card-img-top img-fluid" alt="${meal.strMeal}">
+    <p class="card-text">Meal Origin: ${meal.strArea} </p>
+    <p class="card-text"> Category: ${meal.strCategory} </p>
+    <p class="card-text"> ${meal.strInstructions} </p>
+    <a href="${meal.strYoutube}" class="text-success text-decoration-none" target="_blank">Video recipe</a>
     `;
   mealDetails.appendChild(div);
 };
