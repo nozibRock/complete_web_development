d84436f9f5f74a0155bb5186a297c89229e96d89
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-02-25 23:19:06 +0600

    add data types and primitive

---
+++ milestone_6-js-recap-basic-es6/35_js-interesting-concepts/data-types.js
@@ -0,0 +1,12 @@
+const aNum = 33;
+const age = 13;
+const myAge = 19;
+
+const aNum1 = 19;
+const bStr = 'Jamal Uddin Shahen Shah';
+// let aNum2 = 19;
+/* console.log(aNum2);
+console.log(typeof aNum2); */
+let aNum2 = "Jamal Uddin Shahen Shah";
+console.log(aNum2);
+console.log(typeof aNum2);
