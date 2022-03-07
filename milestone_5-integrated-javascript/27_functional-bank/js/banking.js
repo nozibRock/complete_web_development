4c6cd16d08929c5e97df2967fea0e2e7da33379f
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-03-07 23:13:46 +0600

    add functional bank

--- milestone_5-integrated-javascript/27_functional-bank/js/banking.js
+++ milestone_5-integrated-javascript/27_functional-bank/js/banking.js
@@ -7,40 +7,54 @@ function getInputValue(inputId) {
   return amountValue;
 }
 function updateTotalField(totalFieldId, amount) {
-  debugger;
   const totalElement = document.getElementById(totalFieldId);
   const totalText = totalElement.innerText;
   const previousTotal = parseFloat(totalText);
   totalElement.innerText = previousTotal + amount;
 }
+function getCurrentBalance() {
+  const balanceTotal = document.getElementById("balance-total");
+  const balanceTotalText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(balanceTotalText);
+  return previousBalanceTotal;
+}
+function updateBalance(amount, isAdd) {
+  const balanceTotal = document.getElementById("balance-total");
+  const previousBalanceTotal = getCurrentBalance();
+
+  if (isAdd == true) {
+    balanceTotal.innerText = previousBalanceTotal + amount;
+  } else {
+    balanceTotal.innerText = previousBalanceTotal - amount;
+  }
+}
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
     const depositAmount = getInputValue("deposit-input");
+    if (depositAmount > 0) {
+      // get current deposit
+      updateTotalField("deposit-total", depositAmount);
 
-    // get current deposit
-    updateTotalField("deposit-total", depositAmount);
-
-    // update balance
-    const balanceTotal = document.getElementById("balance-total");
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+      // update balance
+      updateBalance(depositAmount, true);
+    } else {
+      console.log("Please enter a valid amount");
+    }
   });
 
 document
   .getElementById("withdraw-button")
   .addEventListener("click", function () {
     const withdrawAmount = getInputValue("withdraw-input");
-
-    // update withdraw total
-    updateTotalField("withdraw-total", withdrawAmount);
-
-    // update balance
-    const balanceTotal = document.getElementById("balance-total");
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+      // update withdraw total
+      updateTotalField("withdraw-total", withdrawAmount);
+      // update balance
+      updateBalance(withdrawAmount, false);
+    } 
+    if (withdrawAmount > currentBalance) {
+      console.log("You can not withdraw more than what you have in your account");
+    }
   });
