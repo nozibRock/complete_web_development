6ada7a6a1341c3645d645a83c65de7d244d02abb
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-03-07 19:46:18 +0600

    done

---
+++ milestone_7-explore_browser_and_debug/39-broswer-api/alert.js
@@ -0,0 +1,86 @@
+/* console.log("i am here");
+
+// alert("Dowa is coming!!");
+
+const dowaComing = () => {
+  alert("Dowa is coming. hide this now!");
+};
+
+const askPicnic = () => {
+  const response = confirm("Are you going to picnic?");
+  console.log(response);
+  if (response === true) {
+    alert("amake fee ta bKash kore de");
+  } else {
+    console.log("wwmy !!! we will miss you!!!");
+  }
+};
+
+const askName = () => {
+  const name = prompt("What is your name?");
+  if (name) {
+    console.log(name);
+  }
+}; */
+
+
+
+const addItem = () => {
+  const nameField = document.getElementById("product-name");
+  const name = nameField.value;
+  if (!name) {
+    return;
+  }
+  // display in the ui
+  displayProduct(name);
+
+  // add to local storage
+  addProductToCart(name);
+
+  //clear
+  nameField.value = "";
+};
+
+const displayProduct = (name) => {
+  const ulContainer = document.getElementById("products");
+  const li = document.createElement("li");
+  li.innerText = name;
+  ulContainer.appendChild(li);
+};
+
+const getCart = () => {
+  const cart = localStorage.getItem("cart");
+  let cartObj;
+  if (cart) {
+    cartObj = JSON.parse(cart);
+  } else {
+    cartObj = {};
+  }
+  return cartObj;
+};
+
+const addProductToCart = (name) => {
+  const cart = getCart();
+  if (cart[name]) {
+    cart[name] = cart[name] + 1;
+  } else {
+    cart[name] = 1;
+  }
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringified);
+};
+
+const placeOrder = () => {
+  document.getElementById("products").textContent = "";
+  localStorage.removeItem("cart");
+};
+
+
+const displayLocalStorageCart = () => {
+  const cart = getCart();
+  for (const name in cart) {
+    displayProduct(name);
+  }
+};
+
+displayLocalStorageCart();
---
+++ milestone_7-explore_browser_and_debug/39-broswer-api/explore.js
@@ -0,0 +1,14 @@
+const name = 15;
+
+// document.getElementById('name');
+
+const student = {
+  name: "Jolil",
+  friend: "borsha",
+  job: "Business",
+  makeMovie: function (name) {
+    console.log(name);
+  },
+};
+student.makeMovie("The Batman");
+alert("abc");
