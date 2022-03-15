44b788e06dc1129e909e194700cd7c52d99b8be5
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-03-15 11:49:03 +0600

    add data using destructuring and props and Explore destructuring and send data to html elements using props

--- milestone_8-simple_react/44_rest-countries-recap/js/countries.js
+++ milestone_8-simple_react/44_rest-countries-recap/js/countries.js
@@ -6,19 +6,51 @@ const loadCountries = () => {
     .then((data) => displayCountries(data));
 };
 const displayCountries = (countries) => {
-//   console.log(countries);
+  //   console.log(countries);
   console.log(countries[25]);
-  const countriesHTML = countries.map(country => getCountryHTML(country));
-//   console.log(countriesHTML);
-//   console.log(countriesHTML[0]);
-  const container = document.getElementById('countries');
-  container.innerHTML = countriesHTML.join(' ');
+  const countriesHTML = countries.map((country) => getCountryHTML(country));
+  //   console.log(countriesHTML);
+  //   console.log(countriesHTML[0]);
+  const container = document.getElementById("countries");
+  container.innerHTML = countriesHTML.join(" ");
 };
-const getCountryHTML = (country) => {
+
+// original
+/* const getCountryHTML = (country) => {
   return `
         <div class="country">
             <h3>${country.name.common}</h3>
             <img src="${country.flags.svg}" alt="${country.name.common}">
+            <p>Continent: ${country.continents} </p>
+            <p>Area: ${country.area} </p>
+
+        </div>
+    `;
+}; */
+
+// option 1
+/* const getCountryHTML = (country) => {
+    
+    const {name, flags, area, continents} = country;
+  return `
+        <div class="country">
+            <h3>${name.common}</h3>
+            <img src="${flags.svg}" alt="${name.common}">
+            <p>Continent: ${continents} </p>
+            <p>Area: ${area} </p>
+        </div>
+    `;
+}; */
+// option 2
+const getCountryHTML = ({ name, flags, area, continents }) => {
+  return `
+        <div class="country">
+            <h3>${name.common}</h3>
+            <img src="${flags.svg}" alt="${name.common}">
+            
+            <p>Continent: ${continents} </p>
+            <p>Area: ${area} </p>
+
         </div>
     `;
 };
