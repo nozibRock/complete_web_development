44121e8f2c71f88d1d2f7ffd27fe2c7957a17ac3
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-03-17 09:21:23 +0600

    load countries using useEffect

---
+++ milestone_8-simple_react/46_react-rest-countries/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      // .then((data) => console.log(data));
+  }, [])
+  
+  return (
+    <div>
+      <h1>Visiting every Country of the World!!!</h1>
+      <h3>Available Countries: {countries.length} </h3>
+    </div>
+  );
+}
+export default App;
