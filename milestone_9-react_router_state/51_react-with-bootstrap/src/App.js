b8beb3371f7eff4cf4d186ed6ccdf211a7e98aa5
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-03-30 21:08:54 +0600

    up

--- milestone_9-react_router_state/51_react-with-bootstrap/src/App.js
+++ milestone_9-react_router_state/51_react-with-bootstrap/src/App.js
@@ -1,9 +1,18 @@
+import { Button, Spinner } from "react-bootstrap";
 import "./App.css";
+import CardGroup2 from "./components/CardGroup2/CardGroup2";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Using React Bootstrap</h1>
+      <Button variant="outline-warning">My Button</Button>
+      <br />
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+
+      <CardGroup2></CardGroup2>
     </div>
   );
 }
