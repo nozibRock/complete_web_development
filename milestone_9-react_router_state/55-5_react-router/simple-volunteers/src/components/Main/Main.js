b01cc251f699bedbdb58ca992f1a5a8f3e6d2ec9
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-07 23:23:56 +0600

    add search functionalities

--- milestone_9-react_router_state/55-5_react-router/simple-volunteers/src/components/Main/Main.js
+++ milestone_9-react_router_state/55-5_react-router/simple-volunteers/src/components/Main/Main.js
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css'
 
 const Main = () => {
-    const [volunteers, setVolunteers] = useVolunteers();
+    const [volunteers] = useVolunteers();
+
+    const [searchResult, setSearchResult] = useState([]);
+
+    /* const handleChange = (event) => {
+      console.log(event.target.value);
+    } */
+
+    /* const handleSearchChange = (event) => {
+      setSearchText(event.target.value);
+    }; */
+
+    const handleSearchChange2 = event =>{
+        const searchText = event.target.value;
+        const match = volunteers.filter(v => v.title.toLowerCase().includes(searchText));
+        setSearchResult(match);
+    }
     
     return (
       <div>
-        <h2>Volunteer Activities: {volunteers.length}</h2>
+        <h2>Volunteer Activities: {searchResult.length}</h2>
+        <div className="input">
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={handleSearchChange2}
+          />
+        </div>
         <div className="activity-container">
-          {volunteers.map((activity) => (
+          {searchResult.map((activity) => (
             <Activity key={activity._id} activity={activity}></Activity>
           ))}
         </div>
