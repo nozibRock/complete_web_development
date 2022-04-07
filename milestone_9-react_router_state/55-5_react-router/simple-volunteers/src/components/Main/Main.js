fe333efe698f4dccb00396808293cbda81d9b2b8
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-07 23:37:59 +0600

    add done

--- milestone_9-react_router_state/55-5_react-router/simple-volunteers/src/components/Main/Main.js
+++ milestone_9-react_router_state/55-5_react-router/simple-volunteers/src/components/Main/Main.js
@@ -1,44 +1,53 @@
-import { useState } from 'react';
-import useVolunteers from '../../hooks/useVolunteers';
-import Activity from '../Activity/Activity';
-import './Main.css'
+import { useEffect, useState } from "react";
+// import useVolunteers from "../../hooks/useVolunteers";
+import Activity from "../Activity/Activity";
+import "./Main.css";
 
 const Main = () => {
-    const [volunteers] = useVolunteers();
+  // const [volunteers] = useVolunteers();
 
-    const [searchResult, setSearchResult] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
 
-    /* const handleChange = (event) => {
+  const [searchText, setSearchText] = useState("");
+
+  /* const handleChange = (event) => {
       console.log(event.target.value);
     } */
 
-    /* const handleSearchChange = (event) => {
-      setSearchText(event.target.value);
-    }; */
+  useEffect
+  (() => {
+    console.log("inside use Effect");
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const match = data.filter((d) => d.title.toLowerCase().includes(searchText));
+        setSearchResult(match);
+      });
+  }, [searchText]);
 
-    const handleSearchChange2 = event =>{
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  /* const handleSearchChange2 = event =>{
         const searchText = event.target.value;
         const match = volunteers.filter(v => v.title.toLowerCase().includes(searchText));
         setSearchResult(match);
-    }
-    
-    return (
-      <div>
-        <h2>Volunteer Activities: {searchResult.length}</h2>
-        <div className="input">
-          <input
-            type="text"
-            placeholder="Search"
-            onChange={handleSearchChange2}
-          />
-        </div>
-        <div className="activity-container">
-          {searchResult.map((activity) => (
-            <Activity key={activity._id} activity={activity}></Activity>
-          ))}
-        </div>
+    } */
+
+  return (
+    <div>
+      <h2>Volunteer Activities: {searchText.length}</h2>
+      <div className="input">
+        <input type="text" placeholder="Search" onChange={handleSearchChange} />
       </div>
-    );
+      <div className="activity-container">
+        {searchResult.map((activity) => (
+          <Activity key={activity._id} activity={activity}></Activity>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
