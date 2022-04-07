8c02c94f5deed13b3ebfa837df8ffa2bc1f12523
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-07 10:37:38 +0600

    add basic file structure and solution of useRoutes and react version issue

---
+++ milestone_9-react_router_state/55-5_react-router/simple-volunteers/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
+import Volunteers from './components/Volunteers/Volunteers';
+
+function App() {
+  
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path='/volunteers' element={<Volunteers></Volunteers>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ milestone_9-react_router_state/55-5_react-router/simple-volunteers/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  const number = 56;
+
+    return (
+        <div>
+            <button>Click me</button>
+            <br />
+            <h4>My number: {number}</h4>
+        </div>
+    );
+};
+
+export default Home;
