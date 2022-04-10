41bb2a63e5050e57943de1ee7cf2da9c9d1699fe
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-10 11:21:52 +0600

    use bootstrap form and get user email and password

--- milestone_10-react-autentication/57_email-pass-auth-login-form/email-password-auth/src/App.js
+++ milestone_10-react-autentication/57_email-pass-auth-login-form/email-password-auth/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { getAuth } from "firebase/auth";
 import app from './firebase.init';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
@@ -19,14 +21,44 @@ function App() {
     console.log('form submitted');
   }
   return (
-    <div className="App">
-      <form action="" onSubmit={handleFormSubmit}>
-        <input onBlur={handleEmailBlur} type="email" name="" id="" />
-        <br />
-        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
-        <br /> <br />
-        <input type="submit" value="Login" />
-      </form>
+    <div>
+
+      <div className="registration w-50 mx-auto my-5">
+        <h2 className="text-primary">
+          Please {/* {registered ? "Login" : "Register"} */} Register !!
+        </h2>
+        <Form onSubmit={handleFormSubmit}>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="my-3"
+          >
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              onBlur={handleEmailBlur}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </FloatingLabel>
+
+          <FloatingLabel
+            controlId="floatingPassword"
+            label="Password"
+            className="mb-3"
+          >
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onBlur={handlePasswordBlur}
+            />
+          </FloatingLabel>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
