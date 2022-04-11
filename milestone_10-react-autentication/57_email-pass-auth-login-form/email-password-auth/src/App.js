b8da9273b94a4d1305553b7a7900a31bce9dc9ce
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-11 16:44:28 +0600

    add done

--- milestone_10-react-autentication/57_email-pass-auth-login-form/email-password-auth/src/App.js
+++ milestone_10-react-autentication/57_email-pass-auth-login-form/email-password-auth/src/App.js
@@ -1,80 +1,201 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import app from './firebase.init';
-import { Button, FloatingLabel, Form } from 'react-bootstrap';
-import { useState } from 'react';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  updateProfile,
+} from "firebase/auth";
+import app from "./firebase.init";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { useState } from "react";
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
 function App() {
-  const [email, setEmail] = useState('')
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [user, setUser] = useState();
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
+  const [registered, setRegistered] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
+  const toggleLogin = (e) => {
+    setIsLogin(e.target.checked);
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
-  }
+  };
   const handlePasswordBlur = (event) => {
     setPassword(event.target.value);
-  }
-  const handleFormSubmit = event => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // ...
+  };
+  const handleRegisteredChange = (event) => {
+    setRegistered(event.target.checked);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Password Must be at least 6 characters long.");
+      return;
+    }
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError("Password Should contain at least one special character");
+      return;
+    }
+    setValidated(true);
+    setError("");
+
+    if (isLogin) {
+      processLogin(email, password);
+    } else {
+      registerNewUser(email, password);
+    }
+  };
+
+  const processLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
         console.log(user);
+        setError("");
       })
       .catch((error) => {
-        // ..
-        console.error(error)
+        setError(error.message);
       });
-    event.preventDefault();
+  };
 
-    console.log("form submitted", email, password);
-  }
+  const registerNewUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError("");
+        verifyEmail();
+        setUserName();
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const setUserName = () => {
+    updateProfile(auth.currentUser, {
+      displayName: firstName + " " + lastName
+    }).then((result) => {})
+    .catch(error => {
+      setError(error.message);
+    })
+  };
+
+  const handlePasswordReset = () => {
+    sendPasswordResetEmail(auth, email).then(() => {
+      console.log("email sent");
+    });
+  };
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      console.log("Email Verification Sent");
+    });
+  };
+
   return (
-    <div>
+    <div className="registration w-50 mx-auto my-5">
+      <h2 className="text-primary">
+        Please {registered ? "Login" : "Register"} !!
+      </h2>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {!isLogin && (
+          <Row>
+            <Form.Group as={Col} md="6" controlId="validationCustom01">
+              <Form.Label>First name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="First name"
+                onBlur={handleFirstNameChange}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="6" controlId="validationCustom02">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Last name"
+                onBlur={handleLastNameChange}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+        )}
 
-      <div className="registration w-50 mx-auto my-5">
-        <h2 className="text-primary">
-          Please {/* {registered ? "Login" : "Register"} */} Register !!
-        </h2>
-        <Form onSubmit={handleFormSubmit}>
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Email address"
-            className="my-3"
-          >
-            <Form.Control
-              type="email"
-              placeholder="name@example.com"
-              onBlur={handleEmailBlur}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </FloatingLabel>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onBlur={handleEmailBlur}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
 
-          <FloatingLabel
-            controlId="floatingPassword"
-            label="Password"
-            className="mb-3"
-          >
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onBlur={handlePasswordBlur}
-            />
-          </FloatingLabel>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            onChange={(handleRegisteredChange, toggleLogin)}
+            type="checkbox"
+            label="Already Registered?"
+          />
+        </Form.Group>
+
+        <p className="text-danger">{error}</p>
+        <Button onClick={handlePasswordReset} variant="link">
+          Forget Password?
+        </Button>
+        <br />
+        <Button variant="primary" type="submit">
+          {registered ? "Login" : "Register"}
+        </Button>
+      </Form>
     </div>
   );
 }
