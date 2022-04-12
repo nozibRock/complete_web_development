68b9a5885e34bec7eb05cd3e96445cfbf9d8c2cb
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-12 11:20:13 +0600

    add basic react file structure and firebase installation

---
+++ milestone_10-react-autentication/57-5_firebase_recap/tech-geeks-firebase/src/Firebase/Firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4DNkoHRzOINCm_I-PuPMxx6YzqlZ16Ws",
+  authDomain: "tech-geeks-b8a19.firebaseapp.com",
+  projectId: "tech-geeks-b8a19",
+  storageBucket: "tech-geeks-b8a19.appspot.com",
+  messagingSenderId: "1057679020365",
+  appId: "1:1057679020365:web:8930e7b3a795cda7d16704",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
