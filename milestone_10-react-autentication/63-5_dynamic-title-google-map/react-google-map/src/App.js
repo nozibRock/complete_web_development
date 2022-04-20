61c08a2beb94a005a8f5652881c8cedcc19f5d29
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-20 22:55:36 +0600

    unfinished google map project

---
+++ milestone_10-react-autentication/63-5_dynamic-title-google-map/react-google-map/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyLocation from './components/MyLocation';
+
+function App() {
+  return (
+    <div className="App">
+      <MyLocation></MyLocation>
+    </div>
+  );
+}
+
+export default App;
