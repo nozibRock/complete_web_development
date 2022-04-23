fbe9921ac5b81469841f973b09c78082975561a7
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-23 09:19:29 +0600

    up done

--- milestone_11-backend-and-database-integrate/64-getting-started-with-node-express-api/my-smarty-node/index.js
+++ milestone_11-backend-and-database-integrate/64-getting-started-with-node-express-api/my-smarty-node/index.js
@@ -4,10 +4,8 @@ var app = express();
 
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
-app.use(express.json())
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, from my over own personal smarty World!!");
@@ -52,13 +50,17 @@ app.get("/user/:id", (req, res) => {
 }); */
 
 app.get("/users", (req, res) => {
+  console.log('query', req.query);
   res.send(users);
 });
 
-app.post('/user', (req, res) => {
-    console.log('request',  req.body);
-    res.send('post method success')
-})
+app.post("/user", (req, res) => {
+  console.log("request", req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user, "post method success");
+});
 
 app.get("/fruits", (req, res) => {
   res.send(["mango", "apple", "oranges"]);
