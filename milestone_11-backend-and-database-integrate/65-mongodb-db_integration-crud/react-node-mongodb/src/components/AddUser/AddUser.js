402d418d7ffbdd8805867c5c94c949fb83d267a1
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-25 15:04:08 +0600

    send user data to the server and get response to client

---
+++ milestone_11-backend-and-database-integrate/65-mongodb-db_integration-crud/react-node-mongodb/src/components/AddUser/AddUser.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Button, FloatingLabel, Form } from "react-bootstrap";
+
+const AddUser = () => {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email };
+
+    // send data to the server
+    fetch("http://localhost:5000/user/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
+      });
+  };
+  return (
+    <div>
+      <div className="w-50 mx-auto my-5">
+        <h2>Please add a new User</h2>
+
+        <form onSubmit={handleAddUser}>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Name"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+            />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+          >
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="name@example.com"
+              required
+            />
+          </FloatingLabel>
+
+          <Button type="submit">Add User</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
