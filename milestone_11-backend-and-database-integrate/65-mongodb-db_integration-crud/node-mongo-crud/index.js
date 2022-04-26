c6b868e5d9c0db999c941ba2ac4e6f6b9b9e755f
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-26 16:37:02 +0600

    delete a user from the database

--- milestone_11-backend-and-database-integrate/65-mongodb-db_integration-crud/node-mongo-crud/index.js
+++ milestone_11-backend-and-database-integrate/65-mongodb-db_integration-crud/node-mongo-crud/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,12 +26,19 @@ async function run() {
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
 
-    app.get("/user", async(req, res) => {
+    app.get("/user", async (req, res) => {
       // query for movies that have a runtime less than 15 minutes
       const query = {};
       const cursor = userCollection.find(query);
       const users = await cursor.toArray();
       res.send(users);
+    });
+    
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
     });
 
     // POST User : add a new user
@@ -43,15 +50,15 @@ async function run() {
     });
 
     // delete user
-    app.delete('/user/:id', async (req, res) => {
+    app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
       // Query for a movie that has title "Annie Hall"
       const query = { _id: ObjectId(id) };
 
-      
-    })
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
-    await client.close();
   }
 }
 run().catch(console.dir);
