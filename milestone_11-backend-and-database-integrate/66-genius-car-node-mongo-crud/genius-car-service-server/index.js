a761be6017fbb6d6db8d27673165a65dc363aeff
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-27 11:15:45 +0600

    connect to database with secure password on env variable

--- milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-server/index.js
+++ milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-server/index.js
@@ -2,12 +2,25 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 
 const app = express()
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5jrdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Genius Car DB Connected:');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server')
