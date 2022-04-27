331af7d7912d98fb2a68d7f10b4935765544f86e
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-27 06:43:43 +0600

    add done

--- milestone_11-backend-and-database-integrate/65-mongodb-db_integration-crud/react-node-mongodb/src/components/UpdateUser/UpdateUser.js
+++ milestone_11-backend-and-database-integrate/65-mongodb-db_integration-crud/react-node-mongodb/src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, FloatingLabel, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const UpdateUser = () => {
   const { id } = useParams();
+  const [user, setuser] = useState({});
+  useEffect(() => {
+    const url = `http://localhost:5000/user/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setuser(data));
+  }, []);
+
+  const handleUpdateUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const updatedUser = { name, email };
+
+    // send data to the server
+    const url = `http://localhost:5000/user/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
+        alert("users added successfully!!!");
+        event.target.reset();
+      });
+  };
+
   return (
     <div>
-      <h2>Updating User: {id}</h2>
+      <h2>Updating User: {user.name}</h2>
+      <small>User id: {user._id}</small>
+      <p>User email: {user.email}</p>
+
+      <div className="w-50 mx-auto my-5">
+        <Form onSubmit={handleUpdateUser}>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Name"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+            />
+          </FloatingLabel>
+
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+          >
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="name@example.com"
+              required
+            />
+          </FloatingLabel>
+
+          <Button type="submit">Update User</Button>
+        </Form>
+      </div>
     </div>
   );
 };
