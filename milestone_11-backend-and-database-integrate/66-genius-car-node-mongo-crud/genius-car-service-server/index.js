477f3dccca97efb94ca7d9aa25d9ec144fa6c860
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-27 16:29:53 +0600

    load all services and create single service api

--- milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-server/index.js
+++ milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-server/index.js
@@ -1,30 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
-const app = express()
+const app = express();
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5jrdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Genius Car DB Connected:');
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
 
-app.get('/', (req, res) => {
-    res.send('Running Genius Server')
-})
+    const database = client.db("geniusCar");
+    const serviceCollection = database.collection("service");
+    //  const serviceCollection = client.db("geniusCar").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services)
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const service = await serviceCollection.findOne(query);
+      res.send(service)
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Genius Server");
+});
 app.listen(port, () => {
-    console.log('Listening Genius Server port', port)
-})
+  console.log("Listening Genius Server port", port);
+});
