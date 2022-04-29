a9f6dbaa0c0996aa86faeef0efc1fc10dadfe86d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-29 11:53:12 +0600

    insert service data to the mongodb cloud database

--- milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-client/src/Pages/AddService/AddService.js
+++ milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-client/src/Pages/AddService/AddService.js
@@ -5,23 +5,55 @@ const AddService = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    const url = `http://localhost:5000/service`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
   return (
-    <div>
+    <div className="w-50 mx-auto">
       <h2>Please add a service</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
-
+      <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
         {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
+        <input
+          className="mb-2"
+          placeholder="Your Name"
+          {...register("name", { required: true, maxLength: 20 })}
+        />
+        <textarea
+          className="mb-2"
+          placeholder="Description"
+          {...register("description")}
+        />
+        <input
+          className="mb-2"
+          placeholder="Price"
+          type="number"
+          {...register("price")}
+        />
+        <input
+          className="mb-2"
+          placeholder="Photo URL"
+          type="text"
+          {...register("img")}
+        />
         {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+        {errors.name && <span>This field is required</span>}
 
-        <input type="submit" />
+        <input type="submit" value="Add Service" />
       </form>
     </div>
   );
