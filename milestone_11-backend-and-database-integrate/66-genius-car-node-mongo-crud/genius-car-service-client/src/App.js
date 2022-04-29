ae54560d76b4313a03ad9ff9ac3d02f1f8e901f2
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-29 18:15:37 +0600

    manage services and explore delete api

--- milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-client/src/App.js
+++ milestone_11-backend-and-database-integrate/66-genius-car-node-mongo-crud/genius-car-service-client/src/App.js
@@ -7,6 +7,7 @@ import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import ManageServices from './Pages/ManageServices/ManageServices';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -19,20 +20,37 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
-        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+        <Route
+          path="/service/:serviceId"
+          element={<ServiceDetail></ServiceDetail>}
+        ></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/checkout" element={
-          <RequireAuth>
-            <Checkout></Checkout>
-          </RequireAuth>
-        }></Route>
-        <Route path="/add-service" element={
-          <RequireAuth>
-            <AddService></AddService>
-          </RequireAuth>
-        }></Route>
+        <Route
+          path="/checkout"
+          element={
+            <RequireAuth>
+              <Checkout></Checkout>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/add-service"
+          element={
+            <RequireAuth>
+              <AddService></AddService>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/manage-services"
+          element={
+            <RequireAuth>
+              <ManageServices></ManageServices>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
