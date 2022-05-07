7dc43a0e84088c117a2b05e9f5348819f82c56dc
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-07 10:34:35 +0600

    get page count and create pagination button

--- milestone_11-backend-and-database-integrate/67-pagination-load-data-by-filter/ema-john-client-side/src/hooks/useProducts.js
+++ milestone_11-backend-and-database-integrate/67-pagination-load-data-by-filter/ema-john-client-side/src/hooks/useProducts.js
@@ -5,9 +5,9 @@ const useProducts = () =>{
     const [products, setProducts] = useState([]);
 
     useEffect( () =>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data));
+        fetch("http://localhost:5000/product")
+          .then((res) => res.json())
+          .then((data) => setProducts(data));
     }, []);
 
     return [products, setProducts];
