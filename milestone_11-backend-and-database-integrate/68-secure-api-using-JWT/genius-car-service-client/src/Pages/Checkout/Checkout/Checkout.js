511d805515d7927ae8d6c193ec5562cde0a5b090
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-09 15:54:37 +0600

    get order data, create api endpoint and readonly input

--- milestone_11-backend-and-database-integrate/68-secure-api-using-JWT/genius-car-service-client/src/Pages/Checkout/Checkout/Checkout.js
+++ milestone_11-backend-and-database-integrate/68-secure-api-using-JWT/genius-car-service-client/src/Pages/Checkout/Checkout/Checkout.js
@@ -1,44 +1,84 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import useServiceDetail from '../../../hooks/useServiceDetail';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useServiceDetail from "../../../hooks/useServiceDetail";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Checkout = () => {
-    const {serviceId} = useParams();
-    const [service] = useServiceDetail(serviceId);
+  const { serviceId } = useParams();
+  const [service] = useServiceDetail(serviceId);
 
-    const [user, setUser] = useState({
-      name: "Jen Looper",
-      email: 'jenlooper@gmail.com'
-    });
+  const [user] = useAuthState(auth);
 
-    const handleEmailChange = event => {
-        console.log(event.target.value);
-        const {email, ...rest} = user;
-        const newEmail = event.target.value;
-        const newUser = {email: newEmail, ...rest };
-        console.log(newUser);
-        setUser(newUser)
-        console.log(email, rest);
-    }
+  if (user) {
+    console.log(user);
+  }
+  const handlePlaceOrder = (event) => {
+      event.preventDefault();
+      const order = {
+          user: user.displayName,
+          email: user.email,
+          service: service.name,
+          serviceId: serviceId,
+          address: event.target.address.value,
+          phone: event.target.phone.value
+      }
+  };
 
-    return (
-      <div className="w-50 mx-auto">
-        <h2>Please Order: {service.name}</h2>
-        <form action="">
-          <input className='w-100 mb-2' type="text" name="name" value={user.name} placeholder="Name" required />
-          <br />
-          <input className='w-100 mb-2' type="email" name="email" value={user.email} onChange={handleEmailChange} placeholder="Email" required />
-          <br />
-          <input className='w-100 mb-2' type="text" name="service" value={service.name} placeholder="Service" required />
-          <br />
-          <textarea className='w-100 mb-2' type="text" name="address" placeholder="Address" required />
-          <br />
-          <input className='w-100 mb-2' type="number" name="phone" placeholder="Phone" required />
-          <br />
-          <input className='btn btn-success' type="submit" value="Place Order" />
-        </form>
-      </div>
-    );
+  return (
+    <div className="w-50 mx-auto">
+      <h2>Please Order: {service.name}</h2>
+      <form action="">
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="name"
+          value={user.displayName}
+          placeholder="Name"
+          required
+          readOnly
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="email"
+          name="email"
+          value={user.email}
+          placeholder="Email"
+          required
+          readOnly
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="service"
+          value={service.name}
+          placeholder="Service"
+          required
+          readOnly
+        />
+        <br />
+        <textarea
+          className="w-100 mb-2"
+          type="text"
+          name="address"
+          placeholder="Address"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="number"
+          name="phone"
+          placeholder="Phone"
+          required
+        />
+        <br />
+        <input className="btn btn-success" type="submit" value="Place Order" />
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
