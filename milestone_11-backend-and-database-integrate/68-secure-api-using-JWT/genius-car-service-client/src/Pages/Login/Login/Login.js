c0ba7d1fd9a1e66a93d8158d4e62a4693c850af5
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-15 10:03:13 +0600

    send jwt token in the server, verify and decode jwt token

--- milestone_11-backend-and-database-integrate/68-secure-api-using-JWT/genius-car-service-client/src/Pages/Login/Login/Login.js
+++ milestone_11-backend-and-database-integrate/68-secure-api-using-JWT/genius-car-service-client/src/Pages/Login/Login/Login.js
@@ -46,8 +46,7 @@ const Login = () => {
 
         await signInWithEmailAndPassword(email, password);
         const {data} = await axios.post("http://localhost:5000/login", {email});
-        console.log(data)
-        localStorage.setItem('access Token', data.accessToken)
+        localStorage.setItem("accessToken", data.accessToken);
         navigate(from, { replace: true });
 
     }
