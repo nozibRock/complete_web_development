14ea5638bb00eda06bf0fc55b3e3d469e82d4f08
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-05-15 11:07:14 +0600

    Handle 401, 403 to log out user and interceptors

--- milestone_11-backend-and-database-integrate/68-secure-api-using-JWT/genius-car-service-client/src/Pages/Login/RequireAuth/RequireAuth.js
+++ milestone_11-backend-and-database-integrate/68-secure-api-using-JWT/genius-car-service-client/src/Pages/Login/RequireAuth/RequireAuth.js
@@ -8,7 +8,7 @@ import Loading from '../../Shared/Loading/Loading';
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
-    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+    const [sendEmailVerification] = useSendEmailVerification(auth);
     if (loading) {
         return <Loading></Loading>
     }
@@ -16,7 +16,7 @@ const RequireAuth = ({ children }) => {
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
-    console.log(user);
+    // console.log(user);
     if (user.providerData[0]?.providerId === 'password' && !user.emailVerified) {
         return <div className='text-center mt-5'>
             <h3 className='text-danger'>Your Email is not verified!!</h3>
