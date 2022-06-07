d372495af1c7e99d755b8283e06d99db21bbbdeb
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-07 14:24:20 +0600

    71-5 Make Info Card Dynamic with dynamic CSS Class

--- milestone_12-finale/71-doctors-portal-client/doctors-portal-client/src/Pages/Home/Info/Info.js
+++ milestone_12-finale/71-doctors-portal-client/doctors-portal-client/src/Pages/Home/Info/Info.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import InfoCard from './InfoCard/InfoCard';
+import React from "react";
+import InfoCard from "./InfoCard/InfoCard";
 import clock from "../../../assets/icons/clock.svg";
 import marker from "../../../assets/icons/marker.svg";
 import phone from "../../../assets/icons/phone.svg";
 
 const Info = () => {
-    
-    return (
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-        <InfoCard
-          cardTitle="Opening Hours"
-          bgClass="bg-gradient-to-r from-secondary to-primary"
-          img={clock}
-          cardBody="10:00 AM - 10:30 PM"
-        ></InfoCard>
-        <InfoCard
-          cardTitle="Our Locations"
-          bgClass="bg-[#3A4256]"
-          img={marker}
-          cardBody="1st Floor, 50 Purana Paltan,Ruhama Mansion, Dhaka 1000"
-        ></InfoCard>
-        <InfoCard
-          cardTitle="Contact Us"
-          bgClass="bg-gradient-to-r from-secondary to-primary"
-          img={phone}
-          cardBody="+8801711-589670"
-        ></InfoCard>
-      </div>
-    );
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <InfoCard
+        cardTitle="Opening Hours"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={clock}
+        cardText="10:00 AM - 10:30 PM"
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Our Locations"
+        bgClass="bg-accent"
+        img={marker}
+        cardText="1st Floor, 50 Purana Paltan,Ruhama Mansion, Dhaka 1000"
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Contact Us"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={phone}
+        cardText="+8801711-589670"
+      ></InfoCard>
+    </div>
+  );
 };
 
 export default Info;
--- milestone_12-finale/71-doctors-portal-client/doctors-portal-client/src/Pages/Home/Info/InfoCard/InfoCard.js
+++ milestone_12-finale/71-doctors-portal-client/doctors-portal-client/src/Pages/Home/Info/InfoCard/InfoCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const InfoCard = ({ img, cardTitle, cardBody, bgClass }) => {
+const InfoCard = ({ img, cardTitle, cardText, bgClass }) => {
   return (
     <div
       className={`card lg:card-side bg-base-100 shadow-xl hover:scale-105 transition-all ${bgClass}`}
@@ -10,7 +10,7 @@ const InfoCard = ({ img, cardTitle, cardBody, bgClass }) => {
       </figure>
       <div className="card-body text-white">
         <h2 className="card-title">{cardTitle}</h2>
-        <p>{cardBody}</p>
+        <p>{cardText}</p>
       </div>
     </div>
   );
