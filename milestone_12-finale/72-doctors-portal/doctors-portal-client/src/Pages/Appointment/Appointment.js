d61e22d91ee2acbf8268f1e64d40dfcb2a25f84e
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-09 22:40:51 +0600

    72-2 Create Appointment Banner using React Day Picker

--- milestone_12-finale/72-doctors-portal/doctors-portal-client/src/Pages/Appointment/Appointment.js
+++ milestone_12-finale/72-doctors-portal/doctors-portal-client/src/Pages/Appointment/Appointment.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppointmentBanner from './AppointmentBanner/AppointmentBanner';
 
 const Appointment = () => {
     return (
         <div>
-            
+            <AppointmentBanner />
         </div>
     );
 };
