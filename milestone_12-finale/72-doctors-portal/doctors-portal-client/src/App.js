e6fd8d8ce47684ebe20726d16b786dedfc56a2b7
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-09 16:19:37 +0600

    72-1 Module Introduction, Recap and Appointment Page

---
+++ milestone_12-finale/72-doctors-portal/doctors-portal-client/src/App.js
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Loading from "./Pages/Shared/Loading/Loading";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Appointment from "./Pages/Appointment/Appointment";
+
+function App() {
+  return (
+    <div>
+      <Suspense fallback={<Loading />}>
+        <div className="max-w-7xl mx-auto px-12">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/appointment" element={<Appointment />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
