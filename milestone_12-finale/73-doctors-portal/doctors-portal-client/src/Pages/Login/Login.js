7ebeac5b49af75b4aeb5383af99d2674bc065fbf
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-18 14:17:31 +0600

    73-2 Setup Login page, Implement Sign in with Google

--- milestone_12-finale/73-doctors-portal/doctors-portal-client/src/Pages/Login/Login.js
+++ milestone_12-finale/73-doctors-portal/doctors-portal-client/src/Pages/Login/Login.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Login = () => {
-    return (
-      <div>
-        <h1>This is Login page</h1>
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  if (user) {
+    console.log(user);
+  }
+  return (
+    <div className="h-screen flex justify-center items-center">
+      {/* <h1>This is Login page</h1> */}
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Login!</h2>
+          <div className="divider">OR</div>
+
+          <div className="card-actions">
+            <button
+              onClick={() => signInWithGoogle()}
+              className="btn btn-outline"
+            >
+              CONTINUE WITH GOOGLE
+            </button>
+          </div>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
