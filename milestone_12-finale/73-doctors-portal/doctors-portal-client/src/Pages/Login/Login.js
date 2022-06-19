f0dbd90954fc6fc9bdf7be63102a379d703758a6
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-19 22:48:00 +0600

    73-4 Create Login Form using React Hook Form Validation

--- milestone_12-finale/73-doctors-portal/doctors-portal-client/src/Pages/Login/Login.js
+++ milestone_12-finale/73-doctors-portal/doctors-portal-client/src/Pages/Login/Login.js
@@ -20,8 +20,8 @@ const Login = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Login!</h2>
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login!</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control w-full max-w-xs">
               <label className="label">
@@ -32,26 +32,77 @@ const Login = () => {
                 placeholder="Your Email"
                 className="input input-bordered w-full max-w-xs"
                 {...register("email", {
-                  
+                  required: {
+                    value: true,
+                    message: "Email is Required",
+                  },
                   pattern: {
                     value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
                     message: "Provide a valid Email",
                   },
                 })}
               />
-              
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
             </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is Required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be 6 characters or longer",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+
+            <input
+              className="btn w-full max-w-xs text-white bg-accent hover:bg-accent"
+              type="submit"
+              value="Login"
+            />
           </form>
           <div className="divider">OR</div>
 
-          <div className="card-actions">
-            <button
-              onClick={() => signInWithGoogle()}
-              className="btn btn-outline uppercase"
-            >
-              Continue with Google
-            </button>
-          </div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline uppercase"
+          >
+            Continue with Google
+          </button>
         </div>
       </div>
     </div>
