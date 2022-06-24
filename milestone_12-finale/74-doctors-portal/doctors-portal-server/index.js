bce030274fc5b4ac9bf1166db576292f9def42e6
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-06-24 17:42:52 +0600

    74-2 API naming convention and simple Booking API

---
+++ milestone_12-finale/74-doctors-portal/doctors-portal-server/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u4yj0.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const servicesCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    /*
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+     */
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {treatmentId: booking.treatmentId, treatment: booking.treatment, date: booking.date, patient: booking.patient}
+      const result = await bookingCollection.insertOne(booking);
+
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from the Doctors portal!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
